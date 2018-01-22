db5c6d304f49c7ba724ec2e05779a204c00d691f
AlinaKurochka <alinayaremachenko@gmail.com>
2018-01-22 20:24:47 +0200

    start to work

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import Navbar from './sections/Navbar';
+
+import './css/bootstrap.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
